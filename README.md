1f056ccca325b3b0e6f070d6dc8bf2325d93ef99
yanqui <20507951+yanqui-uxo@users.noreply.github.com>
2024-07-01 03:28:48 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Zip an arbitrary number of iterables together
+
+Supports `#![no_std]`
+
+To use, import the `manyzip` function
+
+```
+use manyzip::manyzip
+```
---
+++ src/lib.rs
@@ -0,0 +1,64 @@
+//! Zip an arbitrary number of iterables
+
+#![no_std]
+
+extern crate alloc;
+use alloc::boxed::Box;
+use alloc::vec::Vec;
+
+/// Iterator that zips an arbitrary number of iterables
+///
+/// Created by [manyzip]
+#[derive(Debug)]
+pub struct Manyzip<T: Iterator>(Box<[T]>);
+
+impl<T: Iterator> Iterator for Manyzip<T> {
+    type Item = Vec<T::Item>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.0.is_empty() {
+            return None;
+        }
+        self.0.iter_mut().map(Iterator::next).collect()
+    }
+}
+
+/// Returns an iterator that zips all iterables provided
+///
+/// Yields `None` if any of the iterators yield `None`
+pub fn manyzip<T: IntoIterator>(iterators: T) -> Manyzip<<T::Item as IntoIterator>::IntoIter>
+where
+    T::Item: IntoIterator,
+{
+    Manyzip(iterators.into_iter().map(IntoIterator::into_iter).collect())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use alloc::vec;
+
+    #[test]
+    fn basic() {
+        let test1: Vec<Vec<i32>> =
+            manyzip(vec![vec![1, 2, 3], vec![4, 5], vec![6, 7, 8]]).collect();
+        let test2: Vec<Vec<i32>> = vec![vec![1, 4, 6], vec![2, 5, 7]];
+        assert_eq!(test1, test2);
+    }
+
+    #[test]
+    fn empty_inner() {
+        let test1: Vec<Vec<i32>> = manyzip(vec![vec![], vec![1, 2, 3]]).collect();
+        let test2: Vec<Vec<i32>> = vec![];
+
+        assert_eq!(test1, test2);
+    }
+
+    #[test]
+    fn empty_outer() {
+        let test1: Vec<Vec<i32>> = manyzip::<Vec<Vec<i32>>>(vec![]).collect();
+        let test2: Vec<Vec<i32>> = vec![];
+
+        assert_eq!(test1, test2);
+    }
+}
